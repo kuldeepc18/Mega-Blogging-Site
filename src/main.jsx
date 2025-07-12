d8f2073606784aee6b5e3e7b8cbb3963665e2815
Kuldeep Choudhary <choudharykuldeep1803@gmail.com>
2025-07-13 01:42:47 +0530

    Fix import paths

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,12 @@ import { Provider } from 'react-redux'
 import store from './store/store.js'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Home from './pages/Home.jsx'
+import Home from './Pages/Home.jsx'
 import { AuthLayout, Login } from './components/index.js'
 import AddPost from "./Pages/AddPost.jsx";
-import Signup from './pages/Signup'
-import EditPost from "./pages/EditPost";
-import Post from "./pages/Post";
+import Signup from './Pages/Signup.jsx'
+import EditPost from "./Pages/EditPost.jsx";
+import Post from "./Pages/Post.jsx";
 import AllPosts from "./Pages/AllPosts.jsx";
 
 const router = createBrowserRouter([
